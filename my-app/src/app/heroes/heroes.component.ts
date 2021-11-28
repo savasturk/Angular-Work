9e3e85fc5667e3b447d721c165b97fcc6e8e04c4
savasturk <pinar.savasturk@ceng.deu.edu.tr>
2021-11-28 17:53:26 +0300

    added new components

--- my-app/src/app/heroes/heroes.component.ts
+++ my-app/src/app/heroes/heroes.component.ts
@@ -2,6 +2,9 @@ import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
 import { HEROES } from '../mock-heroes';
 
+import { HeroService } from '../hero.service';
+
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -11,13 +14,20 @@ import { HEROES } from '../mock-heroes';
 export class HeroesComponent implements OnInit {
 
    
- heroes = HEROES;
+ heroes : Hero[] | undefined;
  selectedHero: Hero | undefined ;
 
-  constructor() { }
+  constructor(private heroService: HeroService) { }
 
   ngOnInit(): void {
+    this.getHeroes();
   }
+
+  getHeroes(){
+    this.heroService.getHeroes()
+    .subscribe(heroes => (this.heroes = heroes));
+  }
+
   onSelect(hero: Hero){
     this.selectedHero =  hero;
     console.log(this.selectedHero)
